99a06d49087a472f5194948ee437530bd89b11d6
dmuthami <waruid@gmail.com>
2013-03-07 00:48:39 +0300

    Version 1.0.0.1

--- index.php
+++ index.php
@@ -2,24 +2,17 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-
-        <title>MSC Project</title>
+        <title>Primary Schools in Nairobi County</title>
         <!-- Load external scripts -->
         <script type="text/javascript" src="js/csoMap.js"></script>		
         <script type="text/javascript" src="js/csoWin.js"></script>
         <script type="text/javascript" src="js/csoSearch.js"></script>
-        <script type="text/javascript" src="js/jah.js"></script>
 
-        <!-- Load jQuery and jQuery UI via Google AJAX Libraries API-->
-        <link rel="stylesheet" href="js/themes/base/jquery.ui.all.css">
+        <!-- Load CSS-->
         <link rel="stylesheet" type="text/css" href="css/layout.css">
-         
-        <script src="js/jquery-1.7.2.js"></script>
-        <script src="js/ui/jquery.ui.core.js"></script>
-        <script src="js/ui/jquery.ui.widget.js"></script>
-        <script src="js/ui/jquery.ui.mouse.js"></script>
-        <script src="js/ui/jquery.ui.sortable.js"></script>
-        <script src="js/ui/jquery.ui.accordion.js"></script>
+		
+        <!-- Load jQuery Libraries--> 
+        <script src="lib/jquery/jquery-1.9.1.min.js"></script>
         <script type="text/javascript"> 
             $(document).ready(init);
         </script> 
